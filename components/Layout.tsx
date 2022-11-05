9496ff95cff2bd052df5c962ecbd90df80fda2dd
William Felker <gndclouds@icloud.com>
2022-11-05 19:57:46 +0000

    basic n and f

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,50 +1,20 @@
-import React,{ ReactNode } from 'react'
-import Header from './Header'
+import Navagation from "../components/Navigation";
+import Footer from "../components/Footer";
 
-type Props = {
-  children: ReactNode
+export default function Layout({ children }) {
+  return (
+    <>
+      {/* <script
+        async
+        defer
+        data-website-id="5620b8c4-3a0a-443c-8855-84fb6ccec68b"
+        src="https://umami.tinyfactories.space/umami.js"
+      ></script> */}
+      <div className="mx-auto p-9">
+        <Navagation />
+        <main>{children}</main>
+        <Footer />
+      </div>
+    </>
+  );
 }
-
-const Layout: React.FC<Props> = ( props ) => (
-  <div>
-    <Header />
-    <div className="layout">{props.children}</div>
-    <style jsx global>{`
-      html {
-        box-sizing: border-box;
-      }
-
-      *,
-      *:before,
-      *:after {
-        box-sizing: inherit;
-      }
-
-      body {
-        margin: 0;
-        padding: 0;
-        font-size: 16px;
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
-          Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji',
-          'Segoe UI Symbol';
-        background: rgba(0, 0, 0, 0.05);
-      }
-
-      input,
-      textarea {
-        font-size: 16px;
-      }
-
-      button {
-        cursor: pointer;
-      }
-    `}</style>
-    <style jsx>{`
-      .layout {
-        padding: 0 2rem;
-      }
-    `}</style>
-  </div>
-)
-
-export default Layout
