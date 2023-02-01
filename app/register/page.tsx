944e7a657590144adb7794d111bf4500a4d625fc
William Felker <gndclouds@icloud.com>
2023-02-01 19:47:08 +0000

    MOD

---
+++ app/register/page.tsx
@@ -0,0 +1,20 @@
+import Layout from "../../components/Layout";
+import Form from "../../components/form";
+
+export default function Login() {
+  return (
+    <Layout>
+      <div className="flex h-screen w-screen items-center justify-center bg-gray-50">
+        <div className="z-10 w-full max-w-md overflow-hidden rounded-2xl border border-gray-100 shadow-xl">
+          <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center sm:px-16">
+            <h3 className="text-xl font-semibold">Sign Up</h3>
+            <p className="text-sm text-gray-500">
+              Create an account with your email and password
+            </p>
+          </div>
+          <Form type="register" />
+        </div>
+      </div>
+    </Layout>
+  );
+}
