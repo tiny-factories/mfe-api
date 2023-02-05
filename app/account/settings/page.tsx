b59d09e46366b51ccb9263911affbb71e924f389
William Felker <gndclouds@icloud.com>
2023-02-05 19:49:13 +0000

    removed experments

--- app/account/settings/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex h-screen bg-black">
-      <div className="w-screen h-screen flex flex-col space-y-5 justify-center items-center">
-        Settings
-      </div>
-    </div>
-  );
-}
