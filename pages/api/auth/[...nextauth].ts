4156393715f2672b9b93913b55a412073c50799b
William Felker <gndclouds@icloud.com>
2023-02-05 19:49:02 +0000

    superbase user auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -29,5 +29,11 @@ export default NextAuth({
       },
     }),
   ],
+  callbacks: {
+    async session({ session, user }) {
+      session.user.id = user.id;
+      return session;
+    },
+  },
   session: { strategy: "jwt" },
 });
