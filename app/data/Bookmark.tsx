e7d23b968b3b33db0e0975142233fb6220f004c0
William Felker <gndclouds@icloud.com>
2023-02-18 00:11:56 +0000

    linting bugs

--- app/data/Bookmark.tsx
+++ app/data/Bookmark.tsx
@@ -6,7 +6,6 @@ const BookmarkToggle: React.FC<{}> = ({}) => {
   const [data, setData] = useState(["hello", "hi there", "holla"]);
 
   const [showAll, setShowAll] = useState(false);
-  const [currentIdx, setCurrentIdx] = useState(0);
   const [showCurrent, setShowCurrent] = useState(false);
 
   const toggleAll = () => {
@@ -23,7 +22,6 @@ const BookmarkToggle: React.FC<{}> = ({}) => {
   };
 
   const setCurrent = (index) => {
-    setCurrentIdx(index);
     toggleCurrent();
   };
   return (
@@ -51,13 +49,13 @@ const BookmarkToggle: React.FC<{}> = ({}) => {
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
+                strokeWidth="1.5"
                 stroke="currentColor"
                 className="w-6 h-6"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
                 />
               </svg>
