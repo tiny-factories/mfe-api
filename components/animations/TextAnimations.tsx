ce9b72aff7f3dc16ff0c22ff21c8960236d2ec94
William Felker <gndclouds@icloud.com>
2023-04-17 22:31:23 +0000

    init text animation

---
+++ components/animations/TextAnimations.tsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react";
+import { useInterval } from "react-use";
+import styled, { keyframes } from "styled-components";
+
+export default function TextAnimation() {
+  const reactArray = "cat".split("");
+  const swiftArray = "dog".split("");
+  const [items, setItems] = useState(reactArray);
+  const [count, setCount] = useState(0);
+  const [play, setPlay] = useState(false);
+
+  useInterval(
+	() => {
+	  setItems(reactArray);
+	  setCount(count + 1);
+	  if (count == 1) {
+		setCount(0); 
+		setItems(swiftArray);
+	  }
+	},
+	play ? 6000 : null
+  );
+
+  useEffect(() => {
+	const timer = setTimeout(() => {
+	  setItems(swiftArray);
+	  setPlay(true);
+	}, 4000);
+
+	return () => clearTimeout(timer);
+  }, []);
+
+  return (
+	<Wrapper>
+	  {items.map((item, index) => (
+		<span key={index}>{item}</span>
+	  ))}
+	</Wrapper>
+  );
+}
+
+const animation = keyframes`
+0% { opacity: 0; transform: translateY(-100px) skewY(10deg) skewX(10deg) rotateZ(30deg); filter: blur(10px); }
+25% { opacity: 1; transform: translateY(0px) skewY(0deg) skewX(0deg) rotatez(0deg); filter: blur (0px); }
+75% { opacity: 1; transform: translateY(0px) skewY(0deg) skewX(0deg) rotateZ(0deg); filter: blur(0px); }
+100% { opacity: 0; transform: translateY(-100px) skewY(10deg) skewX(10deg) rotateZ(30deg); filter: blur (10px);}`;
+
+const Wrapper = styled.span`
+	display: inline-block;
+ 
+span {
+	display: inline-block;
+	opacity: 0;
+	animation-name: ${animation};
+	animation-duration: 6s;
+	animation-fill-mode: forwards;
+	animation-iteration-count: infinite;
+}
+span:nth-child(1){
+	animation-delay:0.1s;
+	 
+}
+span:nth-child(2){
+	animation-delay:0.2s;
+	 
+}
+span:nth-child(3){
+	animation-delay:0.4s;
+	 
+}
+span:nth-child(4){
+	animation-delay:0.5s;
+	 
+}`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,43 @@ import Link from "next/link";
 import Image from "next/image";
 import Layout from "../components/Layout";
 import Map from "../components/Map";
+import HeroAnimation from "../components/animations/TextAnimations"
 
 const Home: NextPage = () => {
   const [loading, setLoading] = React.useState(true);
   const handleMapLoading = () => setLoading(false);
-  return <></>;
+  return (
+    <Layout>
+      <div className="text-center uppercase font-bold text-green">
+        <div className="text-h3">We are unlocking</div>
+
+        <div className="text-title">Decades of Climate Data for <HeroAnimation/></div>
+      </div>
+      <div className="">Search</div>
+      <div className="p-4 border border-2 rounded">
+        <form
+          action="https://buttondown.email/api/emails/embed-subscribe/madeforearth"
+          method="post"
+          target="popupwindow"
+          onSubmit="window.open('https://newsletter.madefor.earth', 'popupwindow')"
+          className="flex flex-flex-wrap justify-between items-center"
+        >
+          <label htmlFor="bd-email" className="">
+            Subscrube for updates on our API
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="bd-email"
+            placeholder="hello@madefor.earth"
+            className="grow outline-none border-0 underline underline-offset-2 mx-4"
+          />
+          <input type="submit" value="→" />
+          <input type="hidden" name="tag" value="API → Feature Updates" />
+        </form>
+      </div>
+    </Layout>
+  );
 };
 
 export default Home;
