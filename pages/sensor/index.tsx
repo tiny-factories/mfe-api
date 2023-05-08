3881490842c54e34f3325ff327aea2a56c3d34c9
William Felker <gndclouds@icloud.com>
2023-05-08 22:42:53 +0000

    key typo

--- pages/sensor/index.tsx
+++ pages/sensor/index.tsx
@@ -15,7 +15,7 @@ const Page: React.FC<Props> = (props) => {
         description="Est labore ex esse irure fugiat in anim esse officia sunt irure ipsum ea excepteur tempor. Amet velit veniam ullamco esse ullamco aliquip elit id elit anim exercitation magna culpa. Nostrud et eu duis officia duis ex. Est eu eiusmod est cillum id qui tempor labore excepteur ut. Aliquip ut ea velit in qui velit reprehenderit incididunt non dolor. Laboris do qui veniam do commodo et occaecat irure reprehenderit tempor fugiat."
         color=""
       />
-      <SearchAndData data="sensor" />
+      <SearchAndData dataTable="sensor" />
     </Layout>
   );
 };
